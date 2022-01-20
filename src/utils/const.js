f48b72be85a5b02c5e59ad1ab2df1d1b317f5d88
zyz <845229084@qq.com>
2022-01-20 10:37:50 +0800

    2022.1.20

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,7 +2,7 @@ export default {
 
   install(Vue,options){
     let domainName = '';
-    if(window.location.host != '47.98.38.192:8088'){
+    if(window.location.host.includes('susongzhijia')){
       domainName = 'http://yy.susongzhijia.com/';
     }else{
       // domainName = window.location.protocol+"//"+window.location.host+"/";
