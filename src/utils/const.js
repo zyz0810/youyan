31fb4f3ee6aa099e6de841bba11c3886f3914419
zyz <845229084@qq.com>
2022-01-20 10:34:22 +0800

    2022.1.20

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,10 +2,11 @@ export default {
 
   install(Vue,options){
     let domainName = '';
-    if(window.location.host != '47.98.38.192:8087'){
-      domainName = 'http://yy.susongzhijia.com/'
+    if(window.location.host != '47.98.38.192:8088'){
+      domainName = 'http://yy.susongzhijia.com/';
     }else{
-      domainName = window.location.protocol+"//"+window.location.host+"/";
+      // domainName = window.location.protocol+"//"+window.location.host+"/";
+      domainName = 'http://47.98.38.192:8088/';
     }
     Vue.prototype.global = {
       domainName:domainName
